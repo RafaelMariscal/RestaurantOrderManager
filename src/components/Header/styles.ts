9dddf990063b31c46ec7c4863e3c7e3702c0ebdb
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-11-16 20:52:35 -0300

    Add: Header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  background: #D73035;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 198px;
+  padding: 0 1rem
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 1216px;
+
+  .page-details {
+    h1{
+      color: #fff;
+      font-size: 2rem ;
+    }
+
+    h2{
+      color: #fff;
+      font-size: 1rem;
+      font-weight: 400;
+      opacity: 0.9;
+      margin-top: .375rem
+    }
+  }
+`;
